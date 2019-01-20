1f4d2f372f19efba3431722e6a9c7cbd45b8bef3
James <42484571+smitteh1@users.noreply.github.com>
2019-01-20 10:17:12 -0500

    Update application.java

--- src/live/xjames/frame/application.java
+++ src/live/xjames/frame/application.java
@@ -1,5 +1,22 @@
 package live.xjames.frame;
 
+import javax.swing.*;
+
 public class application {
 
+	private JFrame application;
+	
+	public application() {
+		init();
+	}
+	
+	public void init(){
+		application = new JFrame("SendIt");
+		application.setBounds(500, 500, 250, 600);
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.setVisible(true);
+		application.setLayout(null);
+		application.setResizable(false);
+	}
+	
 }
