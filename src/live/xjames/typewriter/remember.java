21723ec01ea6010e3d6d1bea26dc6a58c5d15bb2
James <42484571+smitteh1@users.noreply.github.com>
2019-01-21 15:45:53 -0500

    Update remember.java

--- src/live/xjames/typewriter/remember.java
+++ src/live/xjames/typewriter/remember.java
@@ -1,5 +1,38 @@
 package live.xjames.typewriter;
 
+import java.io.*;
+
 public class remember {
+
+	private String filePath = "tmp.txt";
+	
+	private String un;
+	private String pass;
+	
+	private File text = new File(filePath);
+	private PrintWriter log;
+	
+	public remember(String username, String password) {
+		un = username;
+		pass = password;
+		startMemory();
+	}
+	
+	private void startMemory() {
+		if (text.exists()) {
+			text.delete();
+			try{text.createNewFile();}catch (IOException e) {e.printStackTrace();}
+			writeMemory();
+		}else {
+			try{text.createNewFile();}catch (IOException e) {e.printStackTrace();}
+			writeMemory();
+		}
+	}
+	
+	private void writeMemory() {
+		try {log = new PrintWriter(text);} catch (FileNotFoundException e) {e.printStackTrace();}
+		log.print(un + ":" + pass);
+		log.close();
+	}
 	
 }
